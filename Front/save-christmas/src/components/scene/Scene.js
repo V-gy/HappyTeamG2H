d383de04e8a02cefee94a61e7e684449a61c7ab6
Fg-35 <fabiengiboire17@gmail.com>
2019-12-19 17:44:57 +0100

    page animation accueil FAITE

--- Front/save-christmas/src/components/scene/Scene.js
+++ Front/save-christmas/src/components/scene/Scene.js
@@ -1,30 +1,21 @@
 import React from "react";
 import "./scene.css";
 import "../../mediaqueries.css"
+import Shinigami from "../shinigami/Shinigami";
 
 class Scene extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {
-            mort: {
-                x: 0,
-                y: 0
-            },
-        };
+        this.state = {};
     }
+
     render() {
         return (
             <div className="dsk-12 scene">
                 <div className="scene-page-off row dsk-1"></div>
                 <div className="scene-page dsk-10"></div>
                 <div className="scene-page-off dsk-1"></div>
-                <img className="addlm" src="./gifs/dieu dela mort/arrivéedelamort.gif" alt=""   /> 
-                <img className="explo" src="./gifs/explosion/explosion.gif" alt=""   />
-                <img className="explo2" src="./gifs/explosion/explosion.gif" alt=""   />
-                <img className="explo3" src="./gifs/explosion/explosion.gif" alt=""   />
-                <img className="explo4" src="./gifs/explosion/explosion.gif" alt=""   />
-                <img className="ddlma" src="./gifs/dieu dela mort/dieudelamortavance.gif" alt=""   />
-                              
+                <Shinigami></Shinigami>
             </div>
         )
     }
