88b760766bf46a09bb87eb0909edfe9312168768
V-gy <vgy@hotmail.fr>
2019-12-19 15:11:49 +0100

    oiseau

--- Front/save-christmas/src/components/scene/Scene.js
+++ Front/save-christmas/src/components/scene/Scene.js
@@ -1,29 +1,19 @@
 import React from "react";
 import "./scene.css";
 import "../../mediaqueries.css"
+import Death from "./Death"
 
 class Scene extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            mort: {
-                x: 0,
-                y: 0
-            },
-        };
-    }
+    
     render() {
         return (
             <div className="dsk-12 scene">
                 <div className="scene-page-off row dsk-1"></div>
-                <div className="scene-page dsk-10"></div>
+                <div className="scene-page dsk-10">
+                    <Death />
+                    </div>
                 <div className="scene-page-off dsk-1"></div>
-                <img className="addlm" src="./gifs/dieu dela mort/arrivéedelamort.gif" alt=""   /> 
-                <img className="explo" src="./gifs/explosion/explosion.gif" alt=""   />
-                <img className="explo2" src="./gifs/explosion/explosion.gif" alt=""   />
-                <img className="explo3" src="./gifs/explosion/explosion.gif" alt=""   />
-                <img className="explo4" src="./gifs/explosion/explosion.gif" alt=""   />
-                <img className="ddlma" src="./gifs/dieu dela mort/dieudelamortavance.gif" alt=""   />
+                
                               
             </div>
         )
