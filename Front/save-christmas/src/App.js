72591a59d64d28ea45237250e04d49775f85e006
V-gy <vgy@hotmail.fr>
2019-12-19 19:25:58 +0100

    attention choice page

--- Front/save-christmas/src/App.js
+++ Front/save-christmas/src/App.js
@@ -4,14 +4,16 @@ import Main from "./components/main/Main";
 import { Switch, Route } from "react-router-dom";
 import Scene from "./components/scene/Scene";
 import SceneEnd from "./components/scene/SceneEnd";
+import Choice from "./components/choice/Choice"
 
 function App() {
   return (
     <div className = "App">
       <Switch>
-        <Route exact path="/" component={Main}/>
-        <Route path="/scene" component={Scene}/>
-        <Route path="/sceneEnd" component={SceneEnd}/>
+        <Route exact path="/" component={Main} />
+        <Route path="/scene" component={Scene} />
+        <Route path="/sceneEnd" component={SceneEnd} />
+        <Route path="/choice" component={Choice} />
       </Switch>
     </div>
   );
