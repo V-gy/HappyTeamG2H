4dc2cac3f8353d2b0cf70eeb4b3f09057f7cec3d
dzendzen <dzen@hotmail.fr>
2019-12-19 19:42:34 +0100

    back and routes,modal en cours de fabrication

--- Front/save-christmas/src/App.js
+++ Front/save-christmas/src/App.js
@@ -1,14 +1,26 @@
 import React from 'react';
+import './App.css';
+import Main from "./components/main/Main";
+import { Switch, Route } from "react-router-dom";
+import './reset.css';
+import Scene from "./components/scene/Scene";
 import SnowPage1 from'./components/SnowPage1';
 import SnowPage2 from'./components/SnowPage2';
+import SnowPage3 from './components/SnowPage3';
 
-import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <SnowPage1 />
-      <SnowPage2 />
+      <Switch>
+       <Route exact path="/" component={Main}/>
+       <Route path="/scene" component={Scene}/>
+      <Route path="/SnowPage1" component={SnowPage1}/>
+      <Route path="/SnowPage2" component={SnowPage2}/>
+      <Route path="/SnowPage3" component={SnowPage3}/>
+      
+      </Switch>
     </div>
   );
 }
