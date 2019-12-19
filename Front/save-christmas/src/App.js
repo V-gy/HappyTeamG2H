18f9509fc725b8684e54e814e22248b7fb6d18b0
V-gy <vgy@hotmail.fr>
2019-12-19 13:15:42 +0100

    sceneend

--- Front/save-christmas/src/App.js
+++ Front/save-christmas/src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Main from "./components/main/Main";
+import { Switch, Route } from "react-router-dom";
+import Scene from "./components/scene/Scene";
+import SceneEnd from "./components/scene/SceneEnd";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = "App">
+      <Switch>
+        <Route exact path="/" component={Main}/>
+        <Route path="/scene" component={Scene}/>
+        <Route path="/sceneEnd" component={SceneEnd}/>
+      </Switch>
     </div>
   );
 }
